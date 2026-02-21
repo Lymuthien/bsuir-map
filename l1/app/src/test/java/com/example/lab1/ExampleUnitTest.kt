e61c72f4af379cbde8508d21bfd8f7827c50b36c
lymuthien <tanya120606@mail.ru>
2026-02-21 12:27:12 +0300

    Added calculator logic tests.

--- l1/app/src/test/java/com/example/lab1/ExampleUnitTest.kt
+++ l1/app/src/test/java/com/example/lab1/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.lab1
 
+import com.example.lab1.calculator.CalculatorEngine
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,7 +12,38 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calculator_add_mul_precedence() {
+        val e = CalculatorEngine()
+        "2+3*4".forEach {
+            when (it) {
+                '+', '-', '*', '/' -> e.inputOperator(it)
+                else -> e.inputDigit(it)
+            }
+        }
+        e.equals()
+        assertEquals("14", e.state().display)
+    }
+
+    @Test
+    fun calculator_decimal_and_division() {
+        val e = CalculatorEngine()
+        e.inputDigit('1')
+        e.inputDecimalPoint()
+        e.inputDigit('5')
+        e.inputOperator('/')
+        e.inputDigit('2')
+        e.equals()
+        assertEquals("0.75", e.state().display)
+    }
+
+    @Test
+    fun calculator_divide_by_zero_is_error() {
+        val e = CalculatorEngine()
+        e.inputDigit('7')
+        e.inputOperator('/')
+        e.inputDigit('0')
+        e.equals()
+        assertEquals("Error", e.state().display)
+        assertTrue(e.state().isError)
     }
 }
